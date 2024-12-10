045ff1ee5a7f29c2cf08e56e08408a4b09efd7a0
osama <usmafia302@gmail.com>
2024-12-10 15:14:15 +0500

    - initial commit

---
+++ lib/screens/auth/auth_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone2/screens/login_screen.dart';
+import 'package:insta_clone2/screens/signup_screen.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPage();
+}
+
+class _AuthPage extends State<AuthPage> {
+  bool a = true;
+  void go() {
+    setState(() {
+      a = !a;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (a) {
+      return LoginScreen(go);
+    } else {
+      return SignupScreen(go);
+    }
+  }
+}
